b5134a71e7b63b4774687240cd0fdd1a1de35966
sudha917 <sudha.avinash917.com>
2018-09-10 17:29:20 +0530

    Collections

---
+++ CollectionsChash/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CollectionsChash
+{
+  
+    class Program
+    {
+        static void Main()
+        {
+             ArrayList da = new ArrayList();
+            //Add
+            da.Add("Visual Basic");
+            da.Add(100);
+            da.Add(55);
+           //Remove
+            da.Remove(55);
+
+            foreach (object ob in da)
+            {
+                Console.WriteLine(ob);
+            }
+            List<string> str = new List<string>();
+            str.Add("Name");
+            str.Add("Address");
+
+            foreach (object obj in str)
+            {
+                if(obj.Equals("Name"))
+                
+                Console.WriteLine(obj);
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}
